b718f4447c8701664e78d64347aa939bb28e5476
jgarms <jgarms@gmail.com>
2023-12-02 12:55:12 -0800

    Part two of day 1

--- go_src/day1/day1.go
+++ go_src/day1/day1.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -20,6 +21,20 @@ func main() {
 		return
 	}
 	fmt.Println("Part one result:", result)
+
+	result, err = partTwo("sample_input.txt")
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	fmt.Println("Part two sample result:", result)
+
+	result, err = partTwo("input.txt")
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	fmt.Println("Part two result:", result)
 }
 
 func readFileLines(filePath string) ([]string, error) {
@@ -81,4 +96,68 @@ func parseCharToInt(char rune) int {
 		return int(char - '0')
 	}
 	return -1
+}
+
+var stringToNum = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+// PrefixToInt if the string starts with an integer string, return the value as an int.
+// Otherwise, return -1
+// e.g. "oneblah" returns 1
+func PrefixToInt(input string) int {
+	for key, value := range stringToNum {
+		if strings.HasPrefix(input, key) {
+			return value
+		}
+	}
+	return -1
+}
+
+func parseLinePartTwo(line string) int {
+	firstNum := -1
+	secondNum := -1
+	for index, c := range line {
+		num := parseCharToInt(c)
+		if num == -1 {
+			num = PrefixToInt(line[index:])
+		}
+		if num > -1 {
+			if firstNum == -1 {
+				firstNum = num
+			} else {
+				secondNum = num
+			}
+		}
+	}
+	if firstNum == -1 {
+		// we didn't find any digits
+		return 0
+	} else if secondNum == -1 {
+		// No second number found, so use the first number twice
+		secondNum = firstNum
+	}
+	return firstNum*10 + secondNum
+}
+
+func partTwo(inputFilePath string) (int, error) {
+	lines, err := readFileLines(inputFilePath)
+	if err != nil {
+		return 0, err
+	}
+	total := 0
+	for _, line := range lines {
+		lineValue := parseLinePartTwo(line)
+		fmt.Println(lineValue)
+		total += lineValue
+	}
+	return total, nil
 }
