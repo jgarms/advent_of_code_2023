ac193e4660b404adc9f309fb0c11f6dfdead1cfe
jgarms <jgarms@gmail.com>
2023-12-03 16:55:03 -0800

    Day 3 part Two

--- day3/day3.go
+++ day3/day3.go
@@ -21,19 +21,28 @@ func main() {
 	}
 	fmt.Println("Part one result:", result)
 
-	//result, err = partTwo("sample_input.txt")
-	//if err != nil {
-	//	fmt.Println("ERROR:", err)
-	//	return
-	//}
-	//fmt.Println("Part two sample result:", result)
-	//
-	//result, err = partTwo("input.txt")
-	//if err != nil {
-	//	fmt.Println("ERROR:", err)
-	//	return
-	//}
-	//fmt.Println("Part two result:", result)
+	result, err = partTwo("sample_input.txt")
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	fmt.Println("Part two sample result:", result)
+
+	result, err = partTwo("input.txt")
+	if err != nil {
+		fmt.Println("ERROR:", err)
+		return
+	}
+	fmt.Println("Part two result:", result)
+}
+
+func partTwo(inputFilePath string) (int, error) {
+	lines, err := utils.ReadFileLines(inputFilePath)
+	if err != nil {
+		return 0, err
+	}
+	schematic := CreateSchematic(lines)
+	return schematic.getGearSum(), nil
 }
 
 func partOne(inputFilePath string) (int, error) {
@@ -42,31 +51,36 @@ func partOne(inputFilePath string) (int, error) {
 		return 0, err
 	}
 	schematic := CreateSchematic(lines)
-	result := schematic.SumValidPartNumbers()
+	result := schematic.getPartSum()
 
 	return result, nil
 }
 
 type Schematic struct {
-	Contents []string
-	MaxX     int
-	MaxY     int
+	Contents           []string
+	MaxX               int
+	MaxY               int
+	PartNumbers        []PartNumber
+	GearsToPartNumbers map[Coordinate][]PartNumber
 }
 
 func CreateSchematic(lines []string) *Schematic {
 	maxY := len(lines) - 1
 	maxX := len(lines[0]) - 1
 
-	return &Schematic{
-		Contents: lines,
-		MaxX:     maxX,
-		MaxY:     maxY,
+	schematic := &Schematic{
+		Contents:           lines,
+		MaxX:               maxX,
+		MaxY:               maxY,
+		PartNumbers:        make([]PartNumber, 0),
+		GearsToPartNumbers: make(map[Coordinate][]PartNumber),
 	}
+	schematic.parse()
+	return schematic
 }
 
-func (schematic *Schematic) SumValidPartNumbers() int {
+func (schematic *Schematic) parse() {
 	var currentPartNumber *PartNumber
-	sum := 0
 	for y := 0; y <= schematic.MaxY; y++ {
 		for x := 0; x <= schematic.MaxX; x++ {
 			c := schematic.Contents[y][x]
@@ -83,14 +97,35 @@ func (schematic *Schematic) SumValidPartNumbers() int {
 				if currentPartNumber != nil {
 					// We've reached the end of a part number
 					if currentPartNumber.IsValid(schematic) {
-						sum += currentPartNumber.GetValue()
+						schematic.PartNumbers = append(schematic.PartNumbers, *currentPartNumber)
+					}
+					coordinate, gearErr := currentPartNumber.GetGearCoordinate(schematic)
+					if gearErr == nil {
+						schematic.GearsToPartNumbers[coordinate] = append(schematic.GearsToPartNumbers[coordinate], *currentPartNumber)
 					}
 					currentPartNumber = nil
 				}
 			}
 		}
 	}
+}
 
+func (schematic *Schematic) getPartSum() int {
+	sum := 0
+	for _, partNumber := range schematic.PartNumbers {
+		sum += partNumber.GetValue()
+	}
+	return sum
+}
+
+func (schematic *Schematic) getGearSum() int {
+	sum := 0
+	for _, partNumbers := range schematic.GearsToPartNumbers {
+		if len(partNumbers) == 2 {
+			product := partNumbers[0].GetValue() * partNumbers[1].GetValue()
+			sum += product
+		}
+	}
 	return sum
 }
 
@@ -118,6 +153,13 @@ func isSymbol(c uint8) bool {
 	return true
 }
 
+func isGearSymbol(c uint8) bool {
+	if c == '*' {
+		return true
+	}
+	return false
+}
+
 type PartNumber struct {
 	X     int
 	Y     int
@@ -140,10 +182,34 @@ func (partNumber PartNumber) IsValid(schematic *Schematic) bool {
 	return false
 }
 
+func (partNumber PartNumber) GetGearCoordinate(schematic *Schematic) (Coordinate, error) {
+	// If a part number has a gear symbol on its border, return it
+	for y := partNumber.Y - 1; y <= partNumber.Y+1; y++ {
+		for x := partNumber.X - 1; x <= partNumber.X+len(partNumber.Value); x++ {
+			c, err := schematic.GetCharAt(x, y)
+			if err != nil {
+				continue
+			}
+			if isGearSymbol(c) {
+				return Coordinate{
+					X: x,
+					Y: y,
+				}, nil
+			}
+		}
+	}
+	return Coordinate{}, fmt.Errorf("No gear symbol found")
+}
+
 func (partNumber PartNumber) GetValue() int {
 	value, err := strconv.Atoi(partNumber.Value)
 	if err != nil {
 		return 0
 	}
 	return value
+}
+
+type Coordinate struct {
+	X int
+	Y int
 }
